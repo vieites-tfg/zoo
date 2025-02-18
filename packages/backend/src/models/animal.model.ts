39223f9febf16eef4d8b8b4d76b23087470b7f2a
Daniel Vieites <dvieitest@gmail.com>
2025-02-18 13:54:33 +0100

    feat: fs structure and model, routes and service impl

---
+++ packages/backend/src/models/animal.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IAnimal {
+  name: string;
+  species: string;
+  birthday: Date;
+  genre: string;
+  diet: string;
+  condition: string;
+  notes: string;
+}
+
+export interface IAnimalDocument extends IAnimal, Document { }
+
+const animalSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    species: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    genre: { type: String, required: true },
+    diet: { type: String, required: true },
+    condition: { type: String, required: true },
+    notes: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Animal = model<IAnimalDocument>('Animal', animalSchema);
