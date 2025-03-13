200a3c97d1ce3f9f6b3dd1376505ffd08850c172
Daniel Vieites <dvieitest@gmail.com>
2025-03-13 16:36:49 +0100

    feat: add new, update and delete tests

--- packages/frontend/cypress/e2e/home.cy.ts
+++ packages/frontend/cypress/e2e/home.cy.ts
@@ -1,11 +1,11 @@
-describe('Página principal - Lista de Animales', () => {
+describe('Main page - Animals list', () => {
   beforeEach(() => {
     cy.intercept('GET', 'http://localhost:3000/animals', { fixture: 'animals.json' }).as('getAnimals')
     cy.visit('/')
+    cy.wait('@getAnimals')
   })
 
-  it('muestra la tabla con los animales', () => {
-    cy.wait('@getAnimals')
+  it('show table with animals', () => {
     cy.get('table').should('exist')
     cy.get('table tbody tr').should('have.length.greaterThan', 0)
     cy.get('table').contains('Leo').should('exist')
