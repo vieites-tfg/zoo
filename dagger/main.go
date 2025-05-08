e571f9f3c4d186e600d9b9759617982c55157bbd
Daniel Vieites <dvieitest@gmail.com>
2025-05-08 13:57:35 +0200

    refactor: better handle of secrets

--- dagger/main.go
+++ dagger/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"context"
 	"dagger/dagger/internal/dagger"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
-type Dagger struct{}
+type Dagger struct{
+	// +optional
+	Sec *dagger.Secret
+}
 
 var secrets Secrets
 
 // Builds the base image from the Dockerfile.
-func (m *Dagger) Base(ctx context.Context, src *dagger.Directory) *dagger.Container {
+func (m *Dagger) Base(ctx context.Context, src *dagger.Directory) (*dagger.Container, error) {
+	if m.Sec != (&dagger.Secret{}) {
+		contents, err := src.File(".env").Contents(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		m.Sec = dagger.Connect().SetSecret("secrets", contents)
+	}
+
 	return src.DockerBuild(dagger.DirectoryDockerBuildOpts{Target: "base"}).
 		WithMountedDirectory("/app", src).
-		WithWorkdir("/app")
+		WithWorkdir("/app"), nil
 }
 
 // Init configures the content with the .env environment variables
@@ -25,17 +33,19 @@ func (m *Dagger) Init(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 ) (*dagger.Container, error) {
-	ctr := m.Base(ctx, src)
-
-	content, err := sec.Plaintext(ctx)
+	ctr, err := m.Base(ctx, src)
 	if err != nil {
 		return nil, err
 	}
 
-	vars := parseEnvFile(content)
-	err = makeSecrets(ctx, vars)
+	content, err := m.Sec.Plaintext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	vars := ParseEnvFile(content)
+	err = MakeSecrets(ctx, vars)
 	if err != nil {
 		return nil, err
 	}
@@ -47,19 +57,18 @@ func (m *Dagger) Init(
 	return ctr, nil
 }
 
-// Returns the backend service.
-func (m *Dagger) LaunchBackend(
+// Returns the backend service with the Mongo database.
+func (m *Dagger) Backend(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 ) (*dagger.Service, error) {
-	ctr, err := m.Init(ctx, src, sec)
+	ctr, err := m.Init(ctx, src)
 	if err != nil {
 		return nil, err
 	}
 
-	mongoPort, err := getMongoPort(ctx)
+	mongoPort, err := GetMongoPort(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -100,13 +109,12 @@ func (m *Dagger) LaunchBackend(
 }
 
 // Returns the frontend service.
-func (m *Dagger) LaunchFrontend(
+func (m *Dagger) Frontend(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 ) (*dagger.Service, error) {
-	_, err := m.Init(ctx, src, sec)
+	_, err := m.Init(ctx, src)
 	if err != nil {
 		return nil, err
 	}
@@ -127,9 +135,8 @@ func (m *Dagger) Lint(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 ) (string, error) {
-	ctr, err := m.Init(ctx, src, sec)
+	ctr, err := m.Init(ctx, src)
 	if err != nil {
 		return "", err
 	}
@@ -140,13 +147,13 @@ func (m *Dagger) Lint(
 		Stdout(ctx)
 }
 
+// Run the tests for the backend.
 func (m *Dagger) TestBackend(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 ) (string, error) {
-	ctr, err := m.Init(ctx, src, sec)
+	ctr, err := m.Init(ctx, src)
 	if err != nil {
 		return "", err
 	}
@@ -156,7 +163,8 @@ func (m *Dagger) TestBackend(
 		Stdout(ctx)
 }
 
-func (m *Dagger) Cypress(ctx context.Context, src *dagger.Directory) *dagger.Container {
+// Returns the Cypress container used to run the e2e tests for the frontend.
+func (m *Dagger) Cypress(src *dagger.Directory) *dagger.Container {
 	return dag.
 		Container().
 		From("cypress/browsers").
@@ -166,152 +174,20 @@ func (m *Dagger) Cypress(ctx context.Context, src *dagger.Directory) *dagger.Con
 		WithExec([]string{"yarn", "add", "lerna@8.2.1", "-W"})
 }
 
+// Run the tests for the frontend.
 func (m *Dagger) TestFrontend(
 	ctx context.Context,
 	// +defaultPath="/"
 	src *dagger.Directory,
-	sec *dagger.Secret,
 	front *dagger.Service,
 ) (string, error) {
-	_, err := m.Init(ctx, src, sec)
+	_, err := m.Init(ctx, src)
 	if err != nil {
 		return "", err
 	}
 
-	return m.Cypress(ctx, src).
+	return m.Cypress(src).
 		WithServiceBinding("zoo-frontend", front).
-		Terminal().
 		WithExec([]string{"yarn", "run", "e2e"}).
 		Stdout(ctx)
-}
-
-func makeSecrets(ctx context.Context, vars map[string]string) error {
-	secrets = make(Secrets)
-	client := dagger.Connect()
-	for key, value := range vars {
-		secrets[key] = client.SetSecret(key, value)
-	}
-
-	_, ok := secrets["MONGO_PORT"]
-	if !ok {
-		secrets["MONGO_PORT"] = client.SetSecret("MONGO_PORT", "27017")
-	}
-
-	mongoUri, err := getMongoUri(ctx)
-	if err != nil {
-		return err
-	}
-	secrets["MONGODB_URI"] = client.SetSecret("MONGODB_URI", mongoUri)
-
-	return nil
-}
-
-func getMongoPort(ctx context.Context) (int, error) {
-	mongo_portStr, err := secrets["MONGO_PORT"].Plaintext(ctx)
-	if err != nil {
-		return 0, err
-	}
-
-	mongo_port, err := strconv.Atoi(mongo_portStr)
-	if err != nil {
-		return 0, err
-	}
-
-	return mongo_port, nil
-}
-
-func getMongoUri(ctx context.Context) (string, error) {
-	var (
-		err       error
-		root      string
-		rootPass  string
-		mongoPort string
-		db        string
-	)
-
-	root, err = secrets.Plaintext(ctx, "MONGO_ROOT")
-	if err != nil {
-		return "", err
-	}
-
-	rootPass, err = secrets.Plaintext(ctx, "MONGO_ROOT_PASS")
-	if err != nil {
-		return "", err
-	}
-
-	mongoPort, err = secrets.Plaintext(ctx, "MONGO_PORT")
-	if err != nil {
-		return "", err
-	}
-
-	db, err = secrets.Plaintext(ctx, "MONGO_DATABASE")
-	if err != nil {
-		return "", err
-	}
-
-	mongoUri := fmt.Sprintf("mongodb://%s:%s@mongodb:%s/%s?authSource=admin",
-		root,
-		rootPass,
-		mongoPort,
-		db,
-	)
-
-	return mongoUri, nil
-}
-
-// parseEnvFile process the content of the .env and returns a variables map
-func parseEnvFile(content string) map[string]string {
-	envVars := make(map[string]string)
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-		parts := strings.SplitN(line, "=", 2)
-		if len(parts) == 2 {
-			key := strings.TrimSpace(parts[0])
-			value := strings.TrimSpace(parts[1])
-			value = strings.Trim(value, "\"'")
-			envVars[key] = value
-		}
-	}
-
-	dct := os.Getenv("DAGGER_CLOUD_TOKEN")
-	if dct != "" {
-		envVars["DAGGER_CLOUD_TOKEN"] = dct
-	}
-
-	return envVars
-}
-
-type Secrets map[string]*dagger.Secret
-
-func (s Secrets) Get(key string) (*dagger.Secret, error) {
-	var (
-		value *dagger.Secret
-		ok    bool
-	)
-	if value, ok = s[key]; !ok {
-		return nil, fmt.Errorf("%s key not set.", key)
-	}
-
-	return value, nil
-}
-
-func (s Secrets) Plaintext(ctx context.Context, key string) (string, error) {
-	var (
-		sec   *dagger.Secret
-		value string
-		err   error
-	)
-	if sec, err = s.Get(key); err != nil {
-		return "", err
-	}
-
-	if value, err = sec.Plaintext(ctx); err != nil {
-		return "", err
-	}
-
-	return value, nil
 }
